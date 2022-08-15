f265205342120971c2118f717a9cca08a420cf5c
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-15 14:14:47 +0330

    Create forms.py in accounts APP & configure settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local APP
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# accounts config
+AUTH_USER_MODEL = 'accounts.CustomUser'
