671165eb601e1ccc74da03096a75c8643763e8c8
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-09-18 19:14:35 +0330

    ADD initial setting for adding Translation in config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# To add Persian Translation uncomment these codes and Comment above codes.
+# LANGUAGE_CODE = 'fa'
+# LANGUAGES = (
+#     ('en', 'English'),
+#     ('fa', 'Persian'),
+# )
+# TIME_ZONE = 'Asia/Tehran'
+# USE_L10N = True
+# USE_I18N = True
+# USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
